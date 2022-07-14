b2f519ee4d6f276d7e84c59bbef2f23a43b6e3b0
srgMet <100075467+srgMet@users.noreply.github.com>
2022-07-14 16:00:23 +0300

    feat: make main page

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+
+const App = () => {
+  return (
+    <dir className='app-wrapper'>
+      <header className='header'>
+        <img src="https://u.to/c0s3HA" />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div> 
+        <div>
+          <a>Settings</a>
+        </div>       
+      </nav>
+      <div className='content'>
+       <img src='https://is.gd/uXnmyh' />
+       </div>
+    </dir>
+  );
+}
+
+
+
+
+export default App;
